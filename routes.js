438a61883a938b07d0644650aee3e1ad4fa09e82
David <darevalo-2022062@kinal.edu.gt>
2024-04-22 15:49:53 -0600

    Se realiza la estructura del pproyecto de a cuerdo a lo solicitado por el PDF

---
+++ routes.js
@@ -0,0 +1,7 @@
+
+const routes = [ 
+    {path:'/auth', /*component: Auth */},
+    {path:'/taskBoard', /*component: TaskBoard */}
+]
+
+export default routes;
---
+++ src/components/ListaTareas.jsx
@@ -0,0 +1,15 @@
+
+/* " En la carpeta src, crea un nuevo
+archivo llamado ListaTareas.jsx. En este archivo, crea un componente llamado ListaTareas que
+renderice una lista de elementos. Usa el estado de React para almacenar los elementos de la
+lista (Array)." */
+
+export const ListaTareas = () => {
+
+    //Lógica para agregar tareas (Formulario)
+
+
+
+    //Lógica para mostar la lista de tareas
+
+};
