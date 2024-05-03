246f32151e1a6954ec9f569c00a951d2be7864da
David Sandoval <jsandoval-2022231@kinal.edu.gt>
2024-05-02 20:00:46 -0600

    a lot errores

--- src/pages/taskBoard/TaskBoardPage.jsx
+++ src/pages/taskBoard/TaskBoardPage.jsx
@@ -3,25 +3,30 @@ import { AddTask } from '../../components/form/AddTask'
 import { Navbar } from '../../components/navbar/Navbar'
 import { useEffect } from 'react';
 import { ListaTareas } from '../../components/ListaTareas';
+
 export const TaskBoardPage = () => {
 
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
-    useEffect(() => {
-        if (!localStorage.getItem('user')) {
-            navigate('/auth');
-        }
-    }, [navigate]);
+    // useEffect(() => {
+    //     if (!localStorage.getItem('user')) {
+    //         navigate('/auth');
+    //     }
+    // }, [navigate]);
 
     return (
-        localStorage.getItem('user') ? (
-            <div>
-                <Navbar />
-                <br />
-                <AddTask />
-                <ListaTareas />
-            </div>
-        ) : null
 
+        <div>
+
+        <AddTask />
+        </div>
+        
+        // localStorage.getItem('user') ? (
+        //     <div>
+        //         <Navbar />
+        //         <br />
+        //         <AddTask />
+        //     </div>
+        // ) : null
     );
 }
