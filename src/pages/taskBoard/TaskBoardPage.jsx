fbdd5624c1667174503229ca882a9d580edb26ee
David Sandoval <jsandoval-2022231@kinal.edu.gt>
2024-05-02 19:40:52 -0600

    errors[C

--- src/pages/taskBoard/TaskBoardPage.jsx
+++ src/pages/taskBoard/TaskBoardPage.jsx
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-import { AddTask } from '../../components/form/AddTask'
-import { ListaTareas } from '../../components/ListaTareas'
-//import './taskBoard.css'
-
-=======
 import { useNavigate } from 'react-router-dom'
 import { AddTask } from '../../components/form/AddTask'
 import { Navbar } from '../../components/navbar/Navbar'
 import { useEffect } from 'react';
->>>>>>> 9f5dbc7f5c627d7069134d32a11d56fba3965496
+import { ListaTareas } from '../../components/ListaTareas';
 export const TaskBoardPage = () => {
 
     const navigate = useNavigate();
@@ -19,21 +13,15 @@ export const TaskBoardPage = () => {
         }
     }, [navigate]);
 
-
     return (
-<<<<<<< HEAD
-        <div className='main-container'>
-            <AddTask/>
-            <ListaTareas className='Lista-Tareas'/>
-        </div>
-=======
         localStorage.getItem('user') ? (
             <div>
                 <Navbar />
                 <br />
                 <AddTask />
+                <ListaTareas />
             </div>
         ) : null
->>>>>>> 9f5dbc7f5c627d7069134d32a11d56fba3965496
-    )
+
+    );
 }
